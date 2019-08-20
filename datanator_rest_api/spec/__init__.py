4a5b025705ad1500d99950adbce5452e3698d8b4
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-08-20 13:02:59 -0400

    Schema organization

--- datanator_rest_api/spec/__init__.py
+++ datanator_rest_api/spec/__init__.py
@@ -5,8 +5,7 @@
 :Copyright: 2019, Karr Lab
 :License: MIT
 """
-
-
+import datanator_rest_api.spec.utils as utils
 if __name__ == "__main__":
 
     utils.parseAPI('.', 'root.yaml', 'DatanatorAPI.yaml')
