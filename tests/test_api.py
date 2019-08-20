2f77cae0d40af197c39e71a95295d269d4f087a3
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-08-20 19:04:03 -0400

    relative paths

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,4 +5,4 @@ from swagger_tester import swagger_test
 class ApiTestCase(unittest.TestCase):
 
     def test_1(self):
-        swagger_test('./spec/DatanatorAPI.yaml')
+        swagger_test('./datanator_rest_api/spec/DatanatorAPI.yaml')
