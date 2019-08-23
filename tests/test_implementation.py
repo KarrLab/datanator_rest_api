6aacf3b2980fbfd0623e0a1dbc9fac41cb5774fb
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-08-23 00:17:32 -0400

    changed from pytest to unnittest format

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -1,22 +1,22 @@
 import connexion
 import pytest
 import datanator_rest_api.core as core
+import unittest
 
 
-AutoResolver = core.AutoResolver
-app = connexion.FlaskApp(__name__)
-app.add_api('../datanator_rest_api/spec/DatanatorAPI.yaml', resolver=AutoResolver(
-    "datanator_rest_api.routes"), validate_responses=False)
+class ImplementationTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.AutoResolver = core.AutoResolver
 
-@pytest.fixture()
-def client():
-    with app.app.test_client() as c:
-        yield c
+        self.app = connexion.FlaskApp(__name__)
+        self.app.add_api('../datanator_rest_api/spec/DatanatorAPI.yaml', resolver=self.AutoResolver(
+            "datanator_rest_api.routes"), validate_responses=False)
+        self.client = self.app.app.test_client()
+        self.client.testing = True
 
-
-def test_routes_implemented(client):
-    response = client.get('/datanator/')
-    print(type(response.data))
-    print(response.data)
-    assert(response.status_code == 200)
+    def test_1(self):
+        response = self.client.get('/datanator/')
+        print(type(response.data))
+        print(response.data)
+        assert(response.status_code == 200)
