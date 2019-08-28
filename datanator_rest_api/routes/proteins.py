083f9f676e15702c7008c5b853c3c9ed4c8bfe20
Zhouyang Lian <zhouyang.lian@familian.life>
2019-08-28 16:24:41 -0400

    protein

--- datanator_rest_api/routes/proteins.py
+++ datanator_rest_api/routes/proteins.py
@@ -41,3 +41,18 @@ class proximity_abundance:
 
     def get(uniprot_id, distance, depth):
         return Manager().manager.get_equivalent_protein(uniprot_id, distance, max_depth=depth)
+
+class meta:
+    
+    def get(species_name=None, protein_name=None, uniprot_id=None, ncbi_taxon_id=None):
+
+        if uniprot_id is not None:  # uniprot_id exists, follow uniprot_id
+            return Manager().manager.get_meta_by_id(uniprot_id)
+        elif protein_name is not None and ncbi_taxon_id is None and species_name is None:  # all proteins with name across organisms
+            return Manager().manager.get_info_by_text(protein_name)
+        elif protein_name is not None and ncbi_taxon_id is not None and species_name is None:  # all proteins with name in taxon_id
+            return Manager().manager.get_meta_by_name_taxon(protein_name, ncbi_taxon_id)
+        elif protein_name is not None and species_name is not None:  # all proteins with name in species_name
+            return Manager().manager.get_meta_by_name_name(protein_name, species_name)
+        else:
+            return 'No such query combination exists'
