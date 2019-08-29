4c8030954f6079964d70e513e2a7dc8ceb977c66
Zhouyang Lian <zhouyang.lian@familian.life>
2019-08-29 15:58:18 -0400

    protein/meta and their tests

--- datanator_rest_api/routes/proteins.py
+++ datanator_rest_api/routes/proteins.py
@@ -42,24 +42,29 @@ class proximity_abundance:
     def get(uniprot_id, distance, depth):
         return Manager().manager.get_equivalent_protein(uniprot_id, distance, max_depth=depth)
 
-class meta_combo:
 
-    def get(uniprot_id=None, ncbi_taxon_id=None, species_name=None, name=None):
-        if uniprot_id is not None:   # uniprot_id
-            return Manager().manager.get_meta_by_id(uniprot_id)
-        elif uniprot_id is not None and ncbi_taxon_id is not None and species_name is None:  # name + taxon_id
-            return Manager().manager.get_meta_by_name_taxon(uniprot_id, ncbi_taxon_id)
-        elif uniprot_id is not None and species_name is not None:   # name + species_name
-            return Manager().manager.get_meta_by_name_name(uniprot_id, species_name)
-        else:
-            return 'Please try another input combination'
+class meta:
 
-class meta_single:
+    class meta_combo:
 
-    def get(name=None, ncbi_taxon_id=None):
-        if name is not None and ncbi_taxon_id is None:  # name only
-            return Manager().manager.get_info_by_text(name)
-        elif name is None and ncbi_taxon_id is not None:  # taxon_id only
-            return Manager().manager.get_info_by_taxonid(ncbi_taxon_id)
-        else:
-            return "This combination of input is not valid."
+        def get(uniprot_id=None, ncbi_taxon_id=None, species_name=None, name=None):
+            if uniprot_id is not None:   # uniprot_id
+                return Manager().manager.get_meta_by_id(uniprot_id)
+            elif name is not None and ncbi_taxon_id is not None and species_name is None:  # name + taxon_id
+                return Manager().manager.get_meta_by_name_taxon(name, ncbi_taxon_id)
+            elif name is not None and species_name is not None:   # name + species_name
+                return Manager().manager.get_meta_by_name_name(name, species_name)
+            else:
+                return 'Please try another input combination'
+
+    class meta_single:
+
+        def get(name=None, ncbi_taxon_id=None, ko=None):
+            if name is not None and ncbi_taxon_id is None and ko is None:  # name only
+                return Manager().manager.get_info_by_text(name)
+            elif name is None and ncbi_taxon_id is not None and ko is None:  # taxon_id only
+                return Manager().manager.get_info_by_taxonid(ncbi_taxon_id)
+            elif name is None and ncbi_taxon_id is None and ko is not None:  # ko only
+                return Manager().manager.get_info_by_ko(ko)
+            else:
+                return "This combination of input is not valid."
--- tests/spec/routes/test_proteins.py
+++ tests/spec/routes/test_proteins.py
@@ -28,3 +28,27 @@ class ImplementationTestCase(unittest.TestCase):
         result_0 = json.loads(self.client.get('/proteins/proximity_abundance/?uniprot_id=Q9D0T1&distance=100&depth=100').data)
         self.assertEqual(len(result_0), 100)
         self.assertEqual(result_0[2]['documents'][0]['uniprot_id'], 'P55770')
+
+    def test_meta(self):
+        result_0 = json.loads(self.client.get(
+            '/proteins/meta/meta_combo/?uniprot_id=Q54ST0,Q9d0t1').data)        
+        self.assertEqual(result_0[0]['ncbi_taxonomy_id'], 44689)
+        self.assertEqual(result_0[1]['ncbi_taxonomy_id'], 10090)
+        result_1 = json.loads(self.client.get(
+            '/proteins/meta/meta_single/?ncbi_taxon_id=9606').data)
+        self.assertEqual(len(result_1[0]['uniprot_ids']), 12412)
+        result_2 = json.loads(self.client.get(
+            '/proteins/meta/meta_single/?name=Nucleoside diphosphate kinase').data)
+        self.assertEqual(len(result_2), 5)
+        result_3 = json.loads(self.client.get(
+            '/proteins/meta/meta_combo/?ncbi_taxon_id=9606&name=alcohol dehydrogenase').data)
+        self.assertEqual(len(result_3), 13)
+        result_4 = json.loads(self.client.get(
+            '/proteins/meta/meta_combo/?ncbi_taxon_id=9606&name=phosphofructokinase&species_name=escherichia coli').data)
+        self.assertEqual(len(result_4), 3)
+        result_5 = json.loads(self.client.get(
+            '/proteins/meta/meta_single/?ncbi_taxon_id=9606&name=alcoo').data)
+        self.assertEqual(result_5, "This combination of input is not valid.")
+        result_6 = json.loads(self.client.get(
+            '/proteins/meta/meta_combo/?ncbi_taxon_id=9606').data)
+        self.assertEqual(result_6, 'Please try another input combination')
