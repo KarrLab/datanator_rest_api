fc306e531f2e5dea44f09b4c993cddddbe904c1c
Zhouyang Lian <zhouyang.lian@familian.life>
2019-08-29 13:36:32 -0400

    module 'datanator_rest_api.routes.proteins' has no attribute 'meta_comboget'

--- datanator_rest_api/routes/proteins.py
+++ datanator_rest_api/routes/proteins.py
@@ -41,3 +41,25 @@ class proximity_abundance:
 
     def get(uniprot_id, distance, depth):
         return Manager().manager.get_equivalent_protein(uniprot_id, distance, max_depth=depth)
+
+class meta_combo:
+
+    def get(species_name=None, name=None, uniprot_id=None, ncbi_taxon_id=None):
+        if uniprot_id is not None:   # uniprot_id
+            return Manager().manager.get_meta_by_id(uniprot_id)
+        elif uniprot_id is not None and ncbi_taxon_id is not None and species_name is None:  # name + taxon_id
+            return Manager().manager.get_meta_by_name_taxon(uniprot_id, ncbi_taxon_id)
+        elif uniprot_id is not None and species_name is not None:   # name + species_name
+            return Manager().manager.get_meta_by_name_name(uniprot_id, species_name)
+        else:
+            return 'Please try another input combination'
+
+class meta_single:
+
+    def get(name=None, ncbi_taxon_id=None):
+        if name is not None and ncbi_taxon_id is None:
+            return Manager().manager.get_info_by_text(name)
+        elif name is None and ncbi_taxon_id is not None:
+            return Manager().manager.get_info_by_taxonid(ncbi_taxon_id)
+        else:
+            return "This combination of input is not valid."
