d074ec4e6717ccc08294ba677b9acf1eeaf23b5c
Zhouyang Lian <zhouyang.lian@familian.life>
2019-09-03 11:53:04 -0400

    removed skeleton code from an earlier version

--- datanator_rest_api/routes/reactions.py
+++ datanator_rest_api/routes/reactions.py
@@ -1,19 +1,9 @@
 """  Reactions Controller
 
 :Author: Bilal Shaikh < bilalshaikh42@gmail.com >
+         Zhouyang Lian < zhouyang.lian@familian.life >
 :Date: 2019-08-21
 :Copyright: 2019, Karr Lab
 :License: MIT
 """
 
-
-def get():
-    return("get")
-
-
-def put(body):
-    return ("put")
-
-
-def post(body):
-    return ("post")
