c224ba13ad2a4195f38ffe47080dd4f50432182e
Zhouyang Lian <zhouyang.lian@familian.life>
2019-09-23 14:38:53 -0400

    spaceDelimited to avoid comma in inchi string conflicting with array

--- tests/routes/test_metabolites.py
+++ tests/routes/test_metabolites.py
@@ -28,6 +28,6 @@ class ImplementationTestCase(unittest.TestCase):
     
     def test_concentrations(self):
         result_0 = self.client.get(
-            '/metabolites/concentrations/?inchi=1S/C10H16N5O13P3/c11-8-5-9(13-2-12-8)15(3-14-5)10-7(17)6(16)4(26-10)1-25-30(21,22)28-31(23,24)27-29(18,19)20/h2-4,6-7,10,16-17H,1H2,(H,21,22)(H,23,24)(H2,11,12,13)(H2,18,19,20)/t4-,6-,7-,10-/m1/s1')
+            '/metabolites/concentrations/?inchi=InChI=1S/C8H11NO2/c9-4-3-6-1-2-7(10)8(11)5-6/h1-2,5,10-11H,3-4,9H2')
         dic_0 = json.loads(result_0.data)
         self.assertEqual(result_0.status_code, 200)
