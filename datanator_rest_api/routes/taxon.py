32821e2ff3199ef76d36295096d0bbca14d2fd5b
zhouyang.lian <zhouyang.lian@familian.life>
2019-10-29 14:54:15 -0400

    routes for taxon queries

---
+++ datanator_rest_api/routes/taxon.py
@@ -0,0 +1,17 @@
+"""  Reactions Controller
+
+:Author: Zhouyang Lian < zhouyang.lian@familian.life >
+:Date: 2019-10-28
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+
+from datanator_query_python.config.query_manager import TaxonManager
+
+
+class canon_rank_distance:
+
+    def get(ncbi_id):
+        results = TaxonManager().txn_manager().get_canon_rank_distance(ncbi_id)
+        return results
---
+++ tests/routes/test_taxon.py
@@ -0,0 +1,20 @@
+import connexion
+import unittest
+import datanator_rest_api.core as core
+import json
+
+class ImplementationTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.AutoResolver = core.AutoResolver
+        self.app = connexion.App(__name__)
+        self.app.add_api('../../datanator_rest_api/spec/DatanatorAPI.yaml', 
+            resolver=self.AutoResolver(
+            "datanator_rest_api.routes"), validate_responses=True)
+        self.client = self.app.app.test_client()
+        self.client.testing = True
+
+    def test_canon_rank_distance(self):
+        result = self.client.get('/taxon/canon_rank_distance/?ncbi_id=9606')
+        self.assertEqual(result.status_code, 200)
