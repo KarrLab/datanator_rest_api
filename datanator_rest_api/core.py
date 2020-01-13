8156b0598cef9821c1c1d055a76f1c67561ebaf3
zhouyang.lian <zhouyang.lian@familian.life>
2020-01-13 15:27:50 -0500

    remove flask_profiler b/c it requires primary node for w operation

--- datanator_rest_api/core.py
+++ datanator_rest_api/core.py
@@ -8,13 +8,11 @@ Provides an automatic implementation of the rest api using the connexion library
 import connexion
 from datanator_rest_api.server import AutoResolver
 from datanator_query_python.config import config
+from pymongo.errors import OperationFailure
 import re
 from flask_cors import CORS
 from swagger_ui_bundle import swagger_ui_3_path
-from flask_profiler import Profiler
 
-
-profiler = Profiler()
 
 def create_app(apiName="DatanatorAPI.yaml", entryModule="datanator_rest_api.routes", 
               specification_dir="./spec", resolver=AutoResolver, validate_responses=False,
@@ -28,12 +26,15 @@ def create_app(apiName="DatanatorAPI.yaml", entryModule="datanator_rest_api.rout
     application.config.from_object(config_class)
     application.config['flask_profiler'] = application.config['FLASKPROFILER']
     CORS(application)
-    profiler.init_app(application)
+    # try:
+    #     profiler.init_app(application)
+    # except OperationFailure:
+    #     return app
     return app
 
 
 if __name__ == "__main__":  # pragma: no cover
 
-    create_app(config_class=config.FlaskProfiler).run(debug=True, host='0.0.0.0', port=8080)
+    create_app(config_class=config.FlaskProfiler).run(host='0.0.0.0', port=8080)
 
 run = create_app(config_class=config.FlaskProfiler)
