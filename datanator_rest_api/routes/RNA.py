88245c64898c0b54f077d24be3890a388d5f3fd0
zhouyang.lian <zhouyang.lian@familian.life>
2020-01-15 15:10:04 -0500

    test_rna.py

--- datanator_rest_api/routes/RNA.py
+++ datanator_rest_api/routes/RNA.py
@@ -9,3 +9,12 @@
 from datanator_query_python.config import query_manager
 
 
+class get_info_by_protein_name:
+
+    def get(protein_name, _from=0, size=0):
+        result = []
+        docs, _ = query_manager.query_rna_halflife.QueryRNA().get_doc_by_protein_name(protein_name, _from=_from,
+                                                                                      size=size)
+        for doc in docs:
+            result.append(doc)
+        return result
