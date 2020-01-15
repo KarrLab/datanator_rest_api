270cedada0cce899dfe666160e34ed7bfe6e04d5
zhouyang.lian <zhouyang.lian@familian.life>
2020-01-15 13:55:07 -0500

    rna.yaml

--- datanator_rest_api/routes/RNA.py
+++ datanator_rest_api/routes/RNA.py
@@ -1,9 +1,11 @@
 """  RNA Controller
 
-:Author: Bilal Shaikh < bilalshaikh42@gmail.com >
-         Zhouyang Lian < zhouyang.lian@familian.life >
+:Author: Zhouyang Lian < zhouyang.lian@familian.life >
 :Date: 2019-08-21
 :Copyright: 2019, Karr Lab
 :License: MIT
 """
+
+from datanator_query_python.config import query_manager
+
 
