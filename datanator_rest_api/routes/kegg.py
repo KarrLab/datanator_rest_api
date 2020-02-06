03c576d3649d6728f143e956ad7cc19bb25d7821
zhouyang.lian <zhouyang.lian@familian.life>
2020-02-06 14:53:31 -0500

    added kegg related routes

---
+++ datanator_rest_api/routes/kegg.py
@@ -0,0 +1,11 @@
+""" Metabolites collection controller
+This file defines the methods for the operations on the Metabolites collection.
+The root class contains the HTTP methods for the /metabolites/ path
+Any subpaths are contained in an internal class
+
+:Author: Zhouyang Lian < zhouyang.lian@familian.life >
+:Date: 2020-02-06
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+from datanator_query_python.config.query_manager import KeggManager
---
+++ tests/routes/test_kegg.py
@@ -0,0 +1,16 @@
+import connexion
+import unittest
+import datanator_rest_api.core as core
+import json
+
+class ImplementationTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.AutoResolver = core.AutoResolver
+        self.app = connexion.App(__name__)
+        self.app.add_api('../../datanator_rest_api/spec/DatanatorAPI.yaml', 
+            resolver=self.AutoResolver(
+            "datanator_rest_api.routes"), validate_responses=True)
+        self.client = self.app.app.test_client()
+        self.client.testing = True
