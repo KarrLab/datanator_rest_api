1f051f1dbc222654d42987b0f67833eaf208d317
zhouyang.lian <zhouyang.lian@familian.life>
2020-03-02 14:18:47 -0500

    use ncbi id instead of species name

--- datanator_rest_api/routes/rna.py
+++ datanator_rest_api/routes/rna.py
@@ -15,15 +15,15 @@ class halflife:
     class get_info_by_protein_name:
 
         def get(protein_name='Protein translocase subunit SecD', _from=0, size=10, 
-        taxon_distance=True, species_name='Homo sapiens'):
+        taxon_distance=True, ncbi_taxonomy_id=9606):
             result = []
             docs, _ = query_manager.RnaManager().rna_manager().get_doc_by_protein_name(protein_name, _from=_from,
                                                                                           size=size)
             for doc in docs:
                 if taxon_distance:
                     for sub_doc in doc['halflives']:
-                        _id = sub_doc['species']
-                        sub_doc['taxon_distance'] = query_manager.TaxonManager().txn_manager().get_common_ancestor(_id, species_name)
+                        _id = sub_doc['ncbi_taxonomy_id']
+                        sub_doc['taxon_distance'] = query_manager.TaxonManager().txn_manager().get_common_ancestor(_id, ncbi_taxonomy_id, org_format='_id')
                 else:
                     result.append(doc)
             return result
