68f0f5baae29535d01a6a7c41de6c28b28500a0f
Zhouyang Lian <zhouyang.lian@familian.life>
2020-05-10 18:50:07 -0400

    remove unnecessary lines

--- datanator_rest_api/core.py
+++ datanator_rest_api/core.py
@@ -8,33 +8,24 @@ Provides an automatic implementation of the rest api using the connexion library
 import connexion
 from datanator_rest_api.server import AutoResolver
 from datanator_query_python.config import config
-from pymongo.errors import OperationFailure
 import re
 from flask_cors import CORS
 from swagger_ui_bundle import swagger_ui_3_path
 
 
 def create_app(apiName="DatanatorAPI.yaml", entryModule="datanator_rest_api.routes", 
-              specification_dir="./spec", resolver=AutoResolver, validate_responses=False,
-              config_class=config.FlaskProfiler):
+              specification_dir="./spec", resolver=AutoResolver, validate_responses=False):
     option = {"swagger_path": swagger_ui_3_path, "swagger_url":"/"}
     app = connexion.App(
         __name__, specification_dir=specification_dir, options=option)
     app.add_api(apiName, resolver=resolver(entryModule),
                 validate_responses=validate_responses, strict_validation=True)
     application = app.app
-    application.config.from_object(config_class)
-    application.config['flask_profiler'] = application.config['FLASKPROFILER']
+
     CORS(application)
-    # try:
-    #     profiler.init_app(application)
-    # except OperationFailure:
-    #     return app
-    return app
 
 
 if __name__ == "__main__":  # pragma: no cover
 
-    create_app(config_class=config.FlaskProfiler).run(host='0.0.0.0', port=8080)
+    create_app().run(host='0.0.0.0', port=8080)
 
-run = create_app(config_class=config.FlaskProfiler)
