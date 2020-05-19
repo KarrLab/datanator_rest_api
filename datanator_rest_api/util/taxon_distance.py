58bcebfc44340b27db0b677714b349b235b1ad72
Zhouyang Lian <zhouyang.lian@familian.life>
2020-05-19 18:02:11 -0400

    get_dist_object

--- datanator_rest_api/util/taxon_distance.py
+++ datanator_rest_api/util/taxon_distance.py
@@ -3,11 +3,11 @@ from datanator_query_python.config import query_manager
 
 class TaxonDist:
 
-    def __init__(self):
-        pass
+    def __init__(self, debugging=False):
+        self.debugging = debugging
 
-    def dist_object(self, doc, queried_species, distance_obj,
-                    name, target_species, org_format='tax_id',
+    def get_dist_object(self, doc, queried_species, distance_obj,
+                    target_species, org_format='tax_id',
                     tax_field='species_name', obj_name='taxon_distance'):
         """Return taxon_distance object for frontend.
 
@@ -15,7 +15,6 @@ class TaxonDist:
             doc(:obj:`Obj`): documents that need to be processed.
             queried_species (:obj:`deque`): already queried species.
             distance_obj (:obj:`Obj`): distance objects containing already queries species.
-            name (:obj:`str` or :obj:`int`): name of species.
             target_species (:obj:`str` or :obj:`int`): target species.
             org_format (:obj:`str`, optional): format of species identifier (tax_id or tax_name). Defaults to 'tax_id'.
             tax_field(:obj:`str`, optional): field containing taxon information in documents.
@@ -24,12 +23,17 @@ class TaxonDist:
         Return:
             (:obj:`tuple` of :obj:`list`, :obj:`Obj`, :obj:`Obj`)
         """
+        self.manager = query_manager.TaxonManager().txn_manager()
         name = doc[tax_field]
         if name not in queried_species:
-            dist = query_manager.TaxonManager().txn_manager().get_canon_common_ancestor(name, target_species, org_format='tax_name')
+            dist = self.manager.get_canon_common_ancestor(name, target_species, org_format=org_format)
             distance_obj[name] = dist
             queried_species.append(name)
-            doc['taxon_distance'] = dist
+            doc[obj_name] = dist
+            if self.debugging:
+                doc['queried'] = True
         else:
-            doc['taxon_distance'] = distance_obj[name]
+            doc[obj_name] = distance_obj[name]
+            if self.debugging:
+                doc['queried'] = False
         return queried_species, distance_obj, doc
---
+++ tests/util/test_taxon_distance.py
@@ -0,0 +1,27 @@
+from datanator_rest_api.util import taxon_distance
+from collections import deque
+import unittest
+
+
+class TestTaxonDist(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.src = taxon_distance.TaxonDist(debugging=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.src.manager.client.close()
+
+    def test_get_dist_object(self):
+        queried_species = deque()
+        distance_obj = {}
+        target_species = 9606
+        tax_field = 'tax_id'
+        docs = [{'tax_id': 2012491}, {'tax_id': 1936271}, {'tax_id': 1841599}, {'tax_id': 1936271}]
+        for i, doc in enumerate(docs):
+            queried_species, distance_obj, doc = self.src.get_dist_object(doc, queried_species, distance_obj,
+                                                                          target_species, tax_field=tax_field)
+            if i == 3:
+                self.assertFalse(doc['queried'])
+        self.assertEqual(len(queried_species), 3)
