4d160e8601b4d6631fe7dec8eb690f3340316bd6
Zhouyang Lian <zhouyang.lian@familian.life>
2020-06-01 17:11:42 -0400

    addressing commandcursor issue

--- datanator_rest_api/routes/reactions.py
+++ datanator_rest_api/routes/reactions.py
@@ -32,9 +32,8 @@ class kinlaw_by_rxn:
                                               skip=_from, limit=size)
         if taxon_distance:
             docs = dist_manager.arrange_distance_objs(docs, target_species=species, tax_field='taxon_name', org_format='tax_name')
-        else:
-            for doc in docs:
-                result.append(doc)
+        for doc in docs:
+            result.append(doc)
         return result
 
 
