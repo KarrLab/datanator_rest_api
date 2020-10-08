1982811cc586746fcdc044c40281fed184eb4536
Zhouyang Lian <zhouyang.lian@familia.life>
2020-10-08 19:41:40 -0400

    use new collection for related_reactions_by_kegg https://github.com/KarrLab/datanator_rest_api/issues/124#issuecomment-705836154

--- datanator_rest_api/routes/proteins.py
+++ datanator_rest_api/routes/proteins.py
@@ -135,9 +135,8 @@ class related:
 
     class related_reactions_by_kegg:
         def get(ko):
-            lists = p_manager.get_info_by_orthodb(ko)
+            lists = p_manager_new.get_info_by_orthodb(ko)
             uniprot_ids = lists[0]['uniprot_ids']
-            print(uniprot_ids)
             kinlaw_ids = query_manager.RxnManager().rxn_manager().get_reaction_by_subunit(uniprot_ids)
             return list(kinlaw_ids)
 
